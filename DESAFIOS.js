5ff13d6d8da3a62919e5cb6879e97e6a73d9b550
Gabriel Braga Ravanelli <gabrielravanelli1@gmail.com>
2026-04-17 12:03:37 -0300

    novos desafios

--- DESAFIOS.js
+++ DESAFIOS.js
@@ -41,14 +41,85 @@ if(operacao === "soma") {
 // $ 6999 a $4008: 7% de bônus 
 // $3999 p baixo: 9% de bônus 
 
-const salarіо = 2000;
+const salarіо = 2000
 
-if(salario >= 11000){
+if(salarіо >= 11000){
     console.log("3% de bônus" )
-} else if(salario < 11000 && salrio >= 7000){
+} else if(salarіо < 11000 && salarіо >= 7000){
     console.log("5% de bônus")
-} else if(salario < 7000 && salrio >= 4000){
+} else if(salarіо < 7000 && salarіо >= 4000){
     console.log("7% de bônus")
 } else{
     console.log("9% de bônus")
 }
+
+// deve ser divisível por 4 mas NÃO divisível por 100
+// OU
+// deve ser divisível por 100 e por 400
+
+const ano = 2024;
+// const ano = 2000;
+// const ano = 2022;
+// const ano = 1992;
+
+if(((ano % 4 === 0) && (ano % 100 !== 0)) || (ano % 400 === 0)){
+    console.log(`${ano} é bissexto`)
+
+}else{
+    console.log(`${ano} não é bissexto`)
+    }
+
+
+// Verificar se estudante receberá bônus na nota.
+// Estudantes recebem bônus se nota for 8 ou acima e se tiverem no máximo 2 faltas. 
+
+const nome = "Roberta";
+const nota = 8;
+const faltas = 2;
+
+const recebeBonus = (nota >= 8) && (faltas <=2) 
+? `${nome} recebe bônus` 
+: `${nome} não recebe bônus` 
+
+console.log(recebeBonus);
+
+// criar um fluxo que identifica o tipo de pessoa usuária e comunica de acordo.
+// ex: pessoa usuária free tem acesso limitado ao app
+//     pessoa usuária premium tem acesso a todas as funções
+//     pessoa usuária super premium tem acesso total e bônus especiais
+
+const user=  `premium`
+
+switch(user){
+    case  `free`:
+        console.log("Acesso limitado");
+        break;
+    case `premium`:
+        console.log("Acesso total ao app");
+        break;
+    case `super premium `:
+        console.log("Acesso total ao app e bônus");
+        break;
+    default:
+        console.log("Tipo de user desconhecido")
+        break;
+}
+
+for(let contador = 1; contador <= 30; contador++){
+    const numero7 = Math.floor(Math.random()*(50 - 1 + 1) + 1);
+    if (numero7 === 15){
+        console.log(`${numero7} em ${contador} tentativas`);
+        break;
+    }
+}
+
+let conta = 0;
+
+for(let i = 1; i <= 30; i++){
+    const numero8 = Math.floor(Math.random()*(50 - 1 + 1) + 1);
+    if(numero8 % 5 === 0 ){
+        continue;
+    }
+    conta++;
+};
+console.log(conta);
